44e64787be01b4df609957cac4a03da186030a53
joseiogr <joseiogr@09220c46-4192-b140-c2c0-8fd757c23b41>
2011-03-25 12:39:11 +0000

    definición de proyectos, falta probar

--- trunk/html/Proyectos/_definirProyecto.php
+++ trunk/html/Proyectos/_definirProyecto.php
@@ -76,13 +76,26 @@ class DefinirProyecto{
 
 		($opciones['id_plantilla'])?$this->opt['id_plantilla'] = $opciones['id_plantilla']:null;
 
+		$Cliente = $var->Proyecto->get_Cliente();
+			foreach($Cliente->get_Lista_Sedes() as $sede){
+				($opciones['definicion_sedes_'.$sede->get_Id().'_horas_desplazamiento'])?$this->opt['definicion_sedes_'.$sede->get_Id().'_horas_desplazamiento']=$opciones['definicion_sedes_'.$sede->get_Id().'_horas_desplazamiento']:null;
+				($opciones['definicion_sedes_'.$sede->get_Id().'_horas_cada_visita'])?$this->opt['definicion_sedes_'.$sede->get_Id().'_horas_cada_visita']=$opciones['definicion_sedes_'.$sede->get_Id().'_horas_cada_visita']:null;
+				($opciones['definicion_sedes_'.$sede->get_Id().'_numero_visitas'])?$this->opt['definicion_sedes_'.$sede->get_Id().'_numero_visitas']=$opciones['definicion_sedes_'.$sede->get_Id().'_numero_visitas']:null;
+				($opciones['definicion_sedes_'.$sede->get_Id().'_gastos_incurridos'])?$this->opt['definicion_sedes_'.$sede->get_Id().'_gastos_incurridos']=$opciones['definicion_sedes_'.$sede->get_Id().'_gastos_incurridos']:null;
+			}
+
 	}
 	
 	private function definir_Proyecto(){
 
+		$this->Proyecto->definir($this->opt);
 		$this->msg = "Proyecto definido";						
 	}
-	
+
+	/**
+	 * Datos a mostrar en la interfaz.
+	 * - Proyectos que son plantillas
+	 */
 	private function obtenerDatos(){
 		$this->ListaProyectos = new ListaProyectos();
 		$filtros['es_plantilla'] = 1;
@@ -90,13 +103,38 @@ class DefinirProyecto{
 		$this->ListaProyectos->buscar($filtros);
 	}
 
+	/**
+	 * Copia la definición de un proyecto en los parámetros de definición del proyecto
+	 *
+	 * Copia primero la definición de cada sede, y posteriormente los datos independientes de las sedes
+	 */
 	private function cargar_Plantilla(){
-		$proyecto = new Proyecto($this->opt['id_plantilla']);
+		$plantilla = new Proyecto($this->opt['id_plantilla']);
 
-		$seguir = true;
-		while($seguir){
+		$cliente_plantilla = $plantilla->get_Cliente();
+		$sedes_plantilla = $cliente_plantilla->get_Lista_Sedes();
 
+		$cliente_proyecto = $this->Proyecto->get_Cliente();
+		$sedes_proyecto = $cliente_proyecto->get_Lista_Sedes();
+
+		if(count($sedes_plantilla) != count($sedes_proyecto))
+			throw new Exception("La plantilla elegida y el proyecto no tienen el mismo número de sedes");
+		
+		$definicion_sedes_plantilla = $plantilla->get_Definicion_Sedes();
+		for($i=0; $i<count($sedes_plantilla);$i++){
+			$sede_plantilla = $sedes_plantilla[$i];
+			$sede_proyecto = $sedes_proyecto[$i];
+
+			$this->opt['definicion_sedes_'.$sede_proyecto->get_Id().'_horas_desplazamiento'] = $definicion_sedes_plantilla[$sede_plantilla->get_Id()]['horas_desplazamiento'];
+			$this->opt['definicion_sedes_'.$sede_proyecto->get_Id().'_horas_cada_visita']	 = $definicion_sedes_plantilla[$sede_plantilla->get_Id()]['horas_cada_visita'];
+			$this->opt['definicion_sedes_'.$sede_proyecto->get_Id().'_numero_visitas']		 = $definicion_sedes_plantilla[$sede_plantilla->get_Id()]['numero_visitas'];
+			$this->opt['definicion_sedes_'.$sede_proyecto->get_Id().'_gastos_incurridos']	 = $definicion_sedes_plantilla[$sede_plantilla->get_Id()]['gastos_incurridos'];
 		}
+
+		$this->opt['nombre'] = $plantilla->get_Nombre();
+		$this->opt['horas_documentacion'] = $plantilla->get_Horas_Documentacion();
+		$this->opt['horas_auditoria_interna'] = $plantilla->get_Horas_Auditoria_Interna();
+		$this->opt['es_plantilla'] = false;
 	}
 }
 ?>
